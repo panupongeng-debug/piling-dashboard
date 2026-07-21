7c28b4c3040762bcd8ce228864ceaad27bd6d30d
panupongeng-debug <panupong.eng@gmail.com>
2026-07-21 18:08:08 +0700

    .

--- update_qc_dashboard.py
+++ update_qc_dashboard.py
@@ -5,7 +5,7 @@ import pandas as pd
 import json
 import os
 
-EXCEL_FILE = r'C:\Users\panup\The Siam Cement Public Company Limited\B-26-6530_RISE Project - STC-REPCO\1.STC to REPCO\5.Others\RISE_Overall RFI_FIR  Summary\RISE_Overall RFI_FIR  Summary.xlsx'
+EXCEL_FILE = r'C:\Users\panup\The Siam Cement Public Company Limited\B-26-6530_RISE Project - STC-REPCO\1.STC to REPCO\5.Others\RISE_Overall RFI_FIR  Summary.xlsx'
 JSON_FILE = r'D:\Daily Report\Rise-dashboard\Data\qc_data.json'
 
 def update_qc_data():
